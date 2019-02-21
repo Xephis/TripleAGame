dd5504a88b95257527619df6deb3252fd6b95769
Stephen King <stephenrichardking@gmail.com>
2019-02-21 16:07:37 +0000

    commit

---
+++ TripleAGame/Monster/IMonster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TripleAGame.Monster
+{
+    interface IMonster
+    {
+        string Name { get; }
+        int Level { get; }
+        int HealthPerLevel { get; }
+
+        // commit
+    }
+}
