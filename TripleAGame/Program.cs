7ded175a0788ae2f203c65f4af74df276b5d527c
Stephen King <stephenrichardking@gmail.com>
2019-02-21 13:02:44 +0000

    Add project files.

---
+++ TripleAGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TripleAGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
